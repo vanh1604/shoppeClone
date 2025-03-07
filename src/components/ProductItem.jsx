479433a9d7692b2264f5d8979be2a2106a2b3445
vanh1604 <VietAnhScout@gmail.com>
2025-03-07 17:27:59 +0700

    finish ui

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { IoLocationOutline } from "react-icons/io5";
+
 const ProductItem = ({
   price,
   name,
@@ -12,60 +13,73 @@ const ProductItem = ({
   discount,
   note,
 }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   const handleStatus = () => {
     if (status) {
       return (
-        <span className="bg-orange-500 text-sm font-semibold text-white p-1 rounded">
+        <span className="bg-orange-500 text-[10px] font-semibold text-white p-1 rounded">
           {status}
         </span>
       );
     }
   };
+
   const discountPrice = Math.round(price * (Number(discount) / 100));
   const formatPrice = discountPrice.toFixed(3);
+
   return (
-    <div className="max-w-sm w-full overflow-hidden shadow-2xl hover:scale-105 transition-all duration-300">
-      <div>
-        <img className="w-full h-80" src={img} alt="" />
-      </div>
-      <div className="p-2 flex flex-col gap-1.5">
-        <p>
-          {handleStatus()} {name}
-        </p>
-        <div className="flex gap-2 items-center">
-          <p className="text-orange-500">
-            <span>₫</span>
-            {formatPrice}
-          </p>
-          <span className="bg-slate-200 text-sm px-2 front-medium text-orange-500 rounded">
-            -{discount}%
-          </span>
+    <div className="relative w-full">
+      <div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        className="max-w-sm relative w-full overflow-hidden shadow-2xl hover:border-2 hover:border-orange-500 hover:scale-102 transition-transform"
+      >
+        <div>
+          <img className="w-full h-80 border-none" src={img} alt="" />
         </div>
-        <p>
-          <div className="flex gap-2">
-            {note.map((item, index) => {
-              return (
+        <div className="p-2 flex flex-col gap-1.5 line-clamp-2 text-ellipsis overflow-hidden">
+          <p className="line-clamp-2 overflow-hidden text-ellipsis">
+            {handleStatus()} {des}
+          </p>
+          <div className="flex gap-2 items-center">
+            <p className="text-orange-500">
+              <span>₫</span>
+              {formatPrice}
+            </p>
+            <span className="bg-slate-200 text-sm px-2 front-medium text-orange-500 rounded">
+              -{discount}%
+            </span>
+          </div>
+          <p>
+            <div className="flex gap-2">
+              {note.map((item, index) => (
                 <span
                   key={index}
                   className="text-[12px] text-orange-500 space-x-1"
                 >
                   {item}
                 </span>
-              );
-            })}
+              ))}
+            </div>
+          </p>
+          <p className="flex items-center gap-2">
+            <span>
+              <FaStar className="text-yellow-300" />
+            </span>
+            {rating}
+          </p>
+          <div className="flex items-center text-slate-600">
+            <IoLocationOutline className="mt-1" />
+            <p>{location}</p>
           </div>
-        </p>
-        <p className="flex items-center gap-2">
-          <span>
-            <FaStar className="text-yellow-300" />
-          </span>
-          {rating}
-        </p>
-        <div className="flex items-center text-slate-600">
-          <IoLocationOutline className="mt-1" />
-          <p>{location}</p>
         </div>
       </div>
+      {isHovered && (
+        <div className="bg-orange-500 scale-102 absolute bottom-[-42px] z-10 text-white text-center py-2 w-full">
+          Tìm kiếm sản phẩm tương tự
+        </div>
+      )}
     </div>
   );
 };
