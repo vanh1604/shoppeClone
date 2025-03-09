4d3acf6dcc3535ce7f3a276ba2e64d4a863c5d48
vanh1604 <VietAnhScout@gmail.com>
2025-03-09 09:48:11 +0700

    finish paging

--- src/App.jsx
+++ src/App.jsx
@@ -1,115 +1,142 @@
-import { useEffect, useState } from "react";
-import { IoIosArrowBack } from "react-icons/io";
-import { IoIosArrowForward } from "react-icons/io";
+import { useState, useMemo, useCallback } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import mockData from "./mockData";
 import ProductItem from "./components/ProductItem";
 import moment from "moment";
 
 function App() {
-  const [data, setData] = useState(mockData);
   const [status, setStatus] = useState("");
-  const [filteredData, setFilteredData] = useState(data);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
 
-  useEffect(() => {
-    let filtered = [...data];
-    if (status === "ban chay") {
-      filtered = filtered.sort((a, b) => a.bestSeller - b.bestSeller);
-    } else if (status === "lien quan") {
-      filtered = filtered.filter((item) => item.rating === 4.5);
-    } else if (status === "low") {
-      filtered = filtered.sort((a, b) => a.price - b.price);
-    } else if (status === "high") {
-      filtered = filtered.sort((a, b) => b.price - a.price);
-    } else if (status === "new") {
-      filtered = filtered.sort((a, b) => {
-        const dateA = moment(a.lastUpdated, "YYYY-MM-DD");
-        const dateB = moment(b.lastUpdated, "YYYY-MM-DD");
-        console.log(dateA, dateB);
-        console.log(dateA - dateB);
+  const filteredData = useMemo(() => {
+    let filtered = [...mockData];
 
-        return dateB - dateA;
-      });
+    switch (status) {
+      case "ban chay":
+        return [...filtered].sort((a, b) => b.bestSeller - a.bestSeller);
+      case "lien quan":
+        return filtered.filter((item) => item.rating >= 4.5);
+      case "low":
+        return [...filtered].sort((a, b) => a.price - b.price);
+      case "high":
+        return [...filtered].sort((a, b) => b.price - a.price);
+      case "new":
+        return [...filtered].sort(
+          (a, b) =>
+            moment(b.lastUpdated, "YYYY-MM-DD") -
+            moment(a.lastUpdated, "YYYY-MM-DD")
+        );
+      default:
+        return filtered;
     }
-    setFilteredData(filtered);
-  }, [status, data]);
+  }, [status]);
 
-  const handlePrice = (e) => {
+  const handlePrice = useCallback((e) => {
     setStatus(e.target.value);
+    setCurrentPage(1);
+  }, []);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
   };
 
   return (
-    <>
-      <div className="container mx-auto">
-        <div className="bg-slate-200 flex max-sm:flex-col justify-between items-center p-2 ">
-          <div className="flex gap-4 max-sm:flex-col">
-            <p className="p-2 cursor-pointer text-slate-700">Sắp xếp theo</p>
-            <p
-              onClick={() => {
-                setStatus("lien quan");
-              }}
-              className={`p-2 cursor-pointer  ${
-                status === "lien quan" ? "bg-orange-500" : "bg-white"
-              } `}
-            >
-              Liên quan
-            </p>
-            <p
-              className="p-2 cursor-pointer bg-white"
-              onClick={() => setStatus("new")}
-            >
-              Mới nhất
-            </p>
-            <p
-              onClick={() => setStatus("ban chay")}
-              className={`p-2 cursor-pointer  ${
-                status === "ban chay" ? "bg-orange-500" : "bg-white"
-              } `}
-            >
-              Bán chạy
-            </p>
-            <select
-              onChange={handlePrice}
-              name="filterPrice"
-              id=""
-              className="bg-white p-2 focus:outline-orange-500"
-            >
-              <option value="low">Giá: Từ thấp đến cao</option>
-              <option value="high">Giá: Từ cao đến thấp</option>
-            </select>
-          </div>
-          <div className="flex items-center">
-            <p className="mr-2">
-              <span className="text-orange-500">1</span>/17
-            </p>
-            <span>
-              <IoIosArrowBack className=" bg-slate-300 text-black text-2xl" />
-            </span>
-            <span>
-              <IoIosArrowForward className=" bg-slate-300 text-black text-2xl" />
-            </span>
-          </div>
+    <div className="container mx-auto">
+      <div className="bg-slate-200 flex max-sm:flex-col justify-between items-center p-2">
+        <div className="flex gap-4 max-sm:flex-col">
+          <p className="p-2 cursor-pointer text-slate-700">Sắp xếp theo</p>
+          <p
+            onClick={() => {
+              setStatus("lien quan");
+              setCurrentPage(1);
+            }}
+            className={`p-2 cursor-pointer ${
+              status === "lien quan" ? "bg-orange-500" : "bg-white"
+            }`}
+          >
+            Liên quan
+          </p>
+          <p
+            onClick={() => {
+              setStatus("new");
+              setCurrentPage(1);
+            }}
+            className="p-2 cursor-pointer bg-white"
+          >
+            Mới nhất
+          </p>
+          <p
+            onClick={() => {
+              setStatus("ban chay");
+              setCurrentPage(1);
+            }}
+            className={`p-2 cursor-pointer ${
+              status === "ban chay" ? "bg-orange-500" : "bg-white"
+            }`}
+          >
+            Bán chạy
+          </p>
+          <select
+            onChange={handlePrice}
+            name="filterPrice"
+            className="bg-white p-2 focus:outline-orange-500"
+          >
+            <option value="low">Giá: Từ thấp đến cao</option>
+            <option value="high">Giá: Từ cao đến thấp</option>
+          </select>
         </div>
-        <div className="grid mt-3 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-4">
-          {filteredData.map((item, index) => {
-            return (
-              <div className="" key={index}>
-                <ProductItem
-                  name={item.name}
-                  price={item.price}
-                  des={item.description}
-                  img={item.image}
-                  rating={item.rating}
-                  location={item.loc}
-                  status={item.status}
-                  note={item.note}
-                  discount={item.discount}
-                />
-              </div>
-            );
-          })}
+        <div className="flex items-center">
+          <p className="mr-2">
+            <span className="text-orange-500">{currentPage}</span>/{totalPages}
+          </p>
+          <span onClick={handlePrevPage}>
+            <IoIosArrowBack
+              className={`bg-slate-300 text-black text-2xl ${
+                currentPage === 1
+                  ? "cursor-not-allowed opacity-50"
+                  : "cursor-pointer"
+              }`}
+            />
+          </span>
+          <span onClick={handleNextPage}>
+            <IoIosArrowForward
+              className={`bg-slate-300 text-black text-2xl ${
+                currentPage === totalPages
+                  ? "cursor-not-allowed opacity-50"
+                  : "cursor-pointer"
+              }`}
+            />
+          </span>
         </div>
       </div>
-    </>
+      <div className="grid mt-3 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-4">
+        {currentItems.map((item, index) => (
+          <ProductItem
+            key={item.id} // Sử dụng item.id thay vì index
+            name={item.name}
+            price={item.price}
+            des={item.description}
+            img={item.image}
+            rating={item.rating}
+            location={item.loc}
+            status={item.status}
+            note={item.note}
+            discount={item.discount}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
